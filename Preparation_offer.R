11f1aa6bc7cb38763861044d452dcb8020f289a4
vigno0405 <164092136+vigno0405@users.noreply.github.com>
2024-04-09 00:31:33 +0200

    Add files via upload

---
+++ Preparation_offer.R
@@ -0,0 +1,18 @@
+# Libraries:
+library(dplyr)
+library(lubridate)
+library(fda)
+library(glmnet)
+library(Matrix)
+library(monotone)
+
+# Loadings to do:
+
+load("offer.RData")
+load("offer.2022.RData")
+load("offer.2023.RData")
+load("offer.clear.RData")
+load("offer.clear.2022.RData")
+load("offer.clear.2023.RData")
+load("prezzozonale.RData")
+load("offer.basis.RData")
